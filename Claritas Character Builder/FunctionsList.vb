6e9ebe87e70b89b71a52629eaf9b8efbb236454a
Chase Harvey <penguin.harvey@gmail.com>
2017-07-04 23:36:49 -0500

    added a second from and some functionality for text setting

---
+++ Claritas Character Builder/FunctionsList.vb
@@ -0,0 +1,21 @@
+﻿Module FunctionsList
+    Function BuildInfoFormLabel(Item As String) As String
+        Dim path As String = My.Application.Info.DirectoryPath
+        Dim Reader As System.IO.StreamReader = My.Computer.FileSystem.OpenTextFileReader(path & "\Character_Builder_Internal_Info.txt")
+        Dim StringReader As String
+        Dim OutputString As String = ""
+        StringReader = Reader.ReadLine
+        While StringReader IsNot "end"
+            If StringReader = Item Then
+                StringReader = Reader.ReadLine
+                While StringReader IsNot Item
+                    OutputString = OutputString & StringReader
+                    StringReader = Reader.ReadLine
+                End While
+            End If
+            StringReader = Reader.ReadLine
+        End While
+        Reader.Close()
+        Return OutputString
+    End Function
+End Module
